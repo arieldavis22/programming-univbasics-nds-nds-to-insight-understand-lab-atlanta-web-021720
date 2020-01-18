83c690c26174330f39197eb452acb26603ee7aa8
Ariel Davis <aadavisbusiness@gmail.com>
2020-01-18 01:43:38 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -9,4 +9,9 @@ def pretty_print_nds(nds)
 end
 
 def print_first_directors_movie_titles
+  row_index = 0
+  while row_index < directors_database.length do
+    p "Testing 123"
+    row_index += 1
+  end
 end
